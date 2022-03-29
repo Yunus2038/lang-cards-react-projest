b2841d94da49075303b74a84217995bb56050b50
Yunus2038 <yns.clk2038@gmail.com>
2022-03-29 22:54:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Card from './component/card/Card';
+import Header from './component/header/Header';
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Card />
+     
+    </div>
+  );
+}
+
+export default App;
